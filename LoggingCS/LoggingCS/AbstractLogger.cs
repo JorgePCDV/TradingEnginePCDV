fc7e1e8a9e8b205e2034674f112a47f03bb2ab54
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-10-17 14:16:34 +0100

    AbstractLogger and ILogger interface initial work with log levels

---
+++ LoggingCS/LoggingCS/AbstractLogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Logging
+{
+    abstract class AbstractLogger : ILogger
+    {
+        protected abstract void Log(LogLevel logLevel, string module, string message);
+
+        public void Debug(string module, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(string module, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string module, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string module, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Information(string module, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Information(string module, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string module, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string module, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LoggingCS/LoggingCS/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TradingEngineServer.Logging
-{
-    public class Class1
-    {
-    }
-}
