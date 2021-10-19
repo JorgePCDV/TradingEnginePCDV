78652310e207ede733040d7958e2d72898791f4f
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-10-19 18:35:50 +0100

    TextLogger implementation init.

---
+++ LoggingCS/LoggingCS/TextLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Logging
+{
+    public class TextLogger : AbstractLogger, ITextLogger
+    {
+        protected override void Log(LogLevel logLevel, string module, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
