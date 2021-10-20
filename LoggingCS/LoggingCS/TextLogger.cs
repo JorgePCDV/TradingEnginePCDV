1386a4896c1f7a9944d8f503c22fe399536aca5d
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-10-20 15:10:43 +0100

    Add TextLogger to dependency injection container

--- LoggingCS/LoggingCS/TextLogger.cs
+++ LoggingCS/LoggingCS/TextLogger.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.Options;
+using System;
+using TradingEngineServer.Logging.LoggingConfiguration;
 
 namespace TradingEngineServer.Logging
 {
     public class TextLogger : AbstractLogger, ITextLogger
     {
+        private readonly LoggerConfiguration _loggerConfiguration;
+        public TextLogger(IOptions<LoggerConfiguration> loggerConfiguration) : base()
+        {
+            _loggerConfiguration = loggerConfiguration.Value ?? throw new ArgumentNullException(nameof(loggerConfiguration));
+        }
         protected override void Log(LogLevel logLevel, string module, string message)
         {
             throw new NotImplementedException();
