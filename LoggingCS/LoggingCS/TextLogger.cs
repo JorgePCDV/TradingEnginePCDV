85f3cc15d21e2486efced2a0b146f6dc72c1b01f
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-11-19 14:06:29 +0000

    Post logging task to BufferBlock

--- LoggingCS/LoggingCS/TextLogger.cs
+++ LoggingCS/LoggingCS/TextLogger.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using System;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
 using TradingEngineServer.Logging.LoggingConfiguration;
 
 namespace TradingEngineServer.Logging
@@ -7,13 +9,16 @@ namespace TradingEngineServer.Logging
     public class TextLogger : AbstractLogger, ITextLogger
     {
         private readonly LoggerConfiguration _loggerConfiguration;
+        private readonly BufferBlock<LogInformation> _logQueue = new BufferBlock<LogInformation>();
+
         public TextLogger(IOptions<LoggerConfiguration> loggerConfiguration) : base()
         {
             _loggerConfiguration = loggerConfiguration.Value ?? throw new ArgumentNullException(nameof(loggerConfiguration));
         }
         protected override void Log(LogLevel logLevel, string module, string message)
         {
-            throw new NotImplementedException();
+            _logQueue.Post(new LogInformation(logLevel, module, message, DateTime.Now, 
+                                Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.Name));
         }
 
         public void Dispose()
