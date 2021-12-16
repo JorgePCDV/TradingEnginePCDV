d9769d2de57c2cdd42a15de678c8769fd53dc3f6
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-16 18:05:31 +0000

    basic OrderCore uniplemented methods

---
+++ OrdersCS/OrderCore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    public class OrderCore : IOrderCore
+    {
+        public long OrderId => throw new NotImplementedException();
+
+        public string Username => throw new NotImplementedException();
+
+        public int SecurityId => throw new NotImplementedException();
+    }
+}
