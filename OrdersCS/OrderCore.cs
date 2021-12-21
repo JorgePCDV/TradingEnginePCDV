18631646a454c1ce0ae6c31c44e91f938d84c2c1
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-21 14:23:50 +0000

    OrderCore

--- OrdersCS/OrderCore.cs
+++ OrdersCS/OrderCore.cs
@@ -6,10 +6,15 @@ namespace TradingEngineServer.Orders
 {
     public class OrderCore : IOrderCore
     {
-        public long OrderId => throw new NotImplementedException();
+        public OrderCore(long orderId, string username, int securityId)
+        {
+            OrderId = orderId;
+            Username = username;
+            SecurityId = securityId;
+        }
 
-        public string Username => throw new NotImplementedException();
-
-        public int SecurityId => throw new NotImplementedException();
+        public long OrderId { get; private set; }
+        public string Username { get; private set; }
+        public int SecurityId { get; private set; }
     }
 }
