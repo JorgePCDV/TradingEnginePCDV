5d46ee6302add6ad9abfbb8626db54af91971168
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-21 15:17:24 +0000

    ModifyOrder initial work

---
+++ OrdersCS/ModifyOrder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Orders
+{
+    class ModifyOrder : IOrderCore
+    {
+        public ModifyOrder(IOrderCore orderCore, 
+                           long modifyPrice,
+                           uint modifyQuantity, 
+                           bool isBuySide)
+        {
+            // PROPERTIES //
+            Price = modifyPrice;
+            Quantity = modifyQuantity;
+            IsBuySide = isBuySide;
+
+            // FIELDS //
+            _orderCore = orderCore;
+        }
+
+        // PROPERTIES //
+        public long Price { get; private set; }
+        public uint Quantity { get; private set; }
+        public bool IsBuySide { get; private set; }
+
+        public long OrderId => _orderCore.OrderId;
+
+        public string Username => _orderCore.Username;
+
+        public int SecurityId => _orderCore.SecurityId;
+
+        // FIELDS //
+        private readonly IOrderCore _orderCore;
+    }
+}
