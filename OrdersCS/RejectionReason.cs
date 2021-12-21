f3c3e278ecd19de43b2519d9100412fcfcf18ddf
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-21 17:42:22 +0000

    RejectionReasons in enum

--- OrdersCS/RejectionReason.cs
+++ OrdersCS/RejectionReason.cs
@@ -6,5 +6,9 @@ namespace TradingEngineServer.Rejects
 {
     public enum RejectionReason
     {
+        Unknown,
+        OrderNotFound,
+        InstrumentNotFound,
+        AttemptingToModifyWrongSide
     }
 }
