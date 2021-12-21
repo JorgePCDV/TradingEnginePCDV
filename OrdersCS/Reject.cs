5447bd8032244d6d26870807d698d2867306c0d0
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-21 16:26:46 +0000

    Reject class fields and properties

---
+++ OrdersCS/Reject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingEngineServer.Orders;
+
+namespace TradingEngineServer.Rejects
+{
+    public class Reject : IOrderCore
+    {
+        public Reject(IOrderCore rejectedOrder, RejectionReason rejectionReason)
+        {
+            // PROPERTIES //
+            RejectionReason = rejectionReason;
+            
+            // FIELDS //
+            _orderCore = rejectedOrder;
+        }
+
+        // PROPERTIES //
+        public RejectionReason RejectionReason { get; private set; }
+        public long OrderId => _orderCore.OrderId;
+        public string Username => _orderCore.Username;
+        public int SecurityId => _orderCore.SecurityId;
+
+
+        // FIELDS //
+        private readonly IOrderCore _orderCore;
+    }
+}
---
+++ OrdersCS/RejectionReason.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingEngineServer.Rejects
+{
+    public enum RejectionReason
+    {
+    }
+}
