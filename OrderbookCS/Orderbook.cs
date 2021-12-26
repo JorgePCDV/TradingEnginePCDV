ef20e5cd68a41e08b8cb52eff69f73e7d299dc85
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-26 17:53:42 +0000

    Orderbook class with IRetrievalOrderbook methods not implemented

--- OrderbookCS/Orderbook.cs
+++ OrderbookCS/Orderbook.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TradingEngineServer.Orders;
 
 namespace TradingEngineServer.Orderbook
 {
     public class Orderbook : IRetrievalOrderbook
     {
+        public int Count => throw new NotImplementedException();
+
+        public void AddOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeOrder(ModifyOrder modifyOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsOrder(long OrderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OrderbookEntry> GetAskOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OrderbookEntry> GetBidOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderbookSpread GetSpread()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveOrder(CancelOrder cancelOrder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
