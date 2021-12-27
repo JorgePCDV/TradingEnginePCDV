a8f508d124a855c5a2380562b2edd9445c1e4afd
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-27 17:32:18 +0000

    Orderbook RemoveOrder

--- OrderbookCS/Orderbook.cs
+++ OrderbookCS/Orderbook.cs
@@ -64,6 +64,52 @@ namespace TradingEngineServer.Orderbook
             }
         }
 
+        public void RemoveOrder(CancelOrder cancelOrder)
+        {
+            if (_orders.TryGetValue(cancelOrder.OrderId, out OrderbookEntry orderBookEntry))
+            {
+                RemoveOrder(cancelOrder.OrderId, orderBookEntry, _orders);
+            }
+        }
+
+        private static void RemoveOrder(long orderId, OrderbookEntry orderBookEntry, Dictionary<long, OrderbookEntry> internalBook)
+        {
+            // Deal with the location of OrderbookEntry within the LinkedList.
+            if(orderBookEntry.Previous != null && orderBookEntry != null)
+            {
+                orderBookEntry.Next.Previous = orderBookEntry.Previous;
+                orderBookEntry.Previous.Next = orderBookEntry.Next;
+            }
+            else if(orderBookEntry.Previous != null)
+            {
+                orderBookEntry.Previous.Next = null;
+            }
+            else if (orderBookEntry.Next != null)
+            {
+                orderBookEntry.Next.Previous = null;
+            }
+            
+            // Deal with OrderbookEntry on Limit-level.
+            if (orderBookEntry.ParentLimit.Head == orderBookEntry && orderBookEntry.ParentLimit.Tail == orderBookEntry)
+            {
+                // Only one order on this Limit-level.
+                orderBookEntry.ParentLimit.Head = null;
+                orderBookEntry.ParentLimit.Tail = null;
+            } 
+            else if (orderBookEntry.ParentLimit.Head == orderBookEntry)
+            {
+                // More than one order, but orderBookEntry is first order on level.
+                orderBookEntry.ParentLimit.Head = orderBookEntry.Next;
+            }
+            else if (orderBookEntry.ParentLimit.Tail == orderBookEntry)
+            {
+                // More than one order, but orderBookEntry is last order on level.
+                orderBookEntry.ParentLimit.Tail = orderBookEntry.Previous;
+            }
+
+            internalBook.Remove(orderId);
+        }
+
         public bool ContainsOrder(long orderId)
         {
             return _orders.ContainsKey(orderId);
@@ -80,11 +126,6 @@ namespace TradingEngineServer.Orderbook
         }
 
         public OrderbookSpread GetSpread()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveOrder(CancelOrder cancelOrder)
         {
             throw new NotImplementedException();
         }
