2d37d9ec50ceb9d7d27448495e25240816dc7f16
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-12-29 15:40:22 +0000

    Target net 5 to be able to use records

--- OrdersCS/OrderRecord.cs
+++ OrdersCS/OrderRecord.cs
@@ -10,11 +10,3 @@ namespace TradingEngineServer.Orders
     public record OrderRecord(long OrderId, uint Quantity, long Price, bool IsBuySide, 
         string Username, int SecurityId, uint TheoreticalQueuePosition);
 }
-
-/// <summary>
-/// This is to enable record types in C#9 due to Visual Studio 2019 bug.
-/// </summary>
-namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit {};
-}
